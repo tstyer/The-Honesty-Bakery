dde15c1a95af80180c3f11f411b8fe8a9f17f278
tstyer <travisstyer.ts@gmail.com>
2026-01-29 16:01:30 +0000

    Update url patterns to adjust for merging front and back

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,15 +15,38 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.static import serve
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('base.urls')),
-    path('', TemplateView.as_view(template_name='index.html'))
+
+    # API (or app) routes first
+    path('api/', include('base.urls')),
+
+    re_path(
+    r"^static/(?P<path>.*)$",
+    serve,
+    {"document_root": settings.BASE_DIR / "frontend" / "build" / "static"},),
+
+    re_path(
+    r"^images/(?P<path>.*)$",
+    serve,
+    {"document_root": settings.BASE_DIR / "frontend" / "build" / "images"},),
+
+    # React catch-all LAST (and it avoids api/static/media)
+    re_path(r'^(?!api/|admin/|static/|media/).*$',
+        TemplateView.as_view(template_name='index.html')),
+
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve static files in development
+urlpatterns += staticfiles_urlpatterns()
+
