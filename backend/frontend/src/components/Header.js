a8147a94e170e33d3d79aa3921d59e130b9d4811
tstyer <travisstyer.ts@gmail.com>
2026-02-09 13:07:03 +0000

    Remove react-router-bootstrap and replace with react-router-dom Link

--- backend/frontend/src/components/Header.js
+++ backend/frontend/src/components/Header.js
@@ -1,152 +1,126 @@
-import React from "react";
-import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
-import { NavLink } from "react-router-dom";
-import { LinkContainer } from "react-router-bootstrap";
-import { useDispatch, useSelector } from "react-redux";
-import { logout } from "../actions/userActions";
+import React from 'react'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap'
+import { NavLink, Link } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux'
+import { logout } from '../actions/userActions'
 
 function Header() {
-  const dispatch = useDispatch();
+  const dispatch = useDispatch()
 
-  const userLogin = useSelector((state) => state.userLogin);
-  const { userInfo } = userLogin;
+  const { userInfo } = useSelector((state) => state.userLogin)
 
   // Pull cart items to show a live cart badge
-  const cart = useSelector((state) => state.cart);
-  const { cartItems } = cart;
+  const { cartItems } = useSelector((state) => state.cart)
 
-  // Total items in cart 
-  const cartItemCount = cartItems.reduce((acc, item) => acc + item.qty, 0);
+  // Total items in cart
+  const cartItemCount = cartItems.reduce((acc, item) => acc + item.qty, 0)
 
   const logoutHandler = () => {
-    dispatch(logout());
-  };
+    dispatch(logout())
+  }
 
   return (
     <header>
       <Navbar className="brand-bg" expand="lg" collapseOnSelect>
         <Container>
-          {/* Logo */}
-          <LinkContainer to="/">
-            <Navbar.Brand>
-              <img
-                src="/images/logo_2.png"
-                alt="Business logo"
-                className="logo"
-              />
-            </Navbar.Brand>
-          </LinkContainer>
+          {/* Logo / Brand */}
+          <Navbar.Brand as={Link} to="/">
+            <img src="/images/logo_2.png" alt="The Honesty Bakehouse logo" className="logo" />
+          </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
 
           <Navbar.Collapse id="basic-navbar-nav">
             {/* LEFT NAV */}
             <Nav className="me-auto">
-              <NavLink
+              <Nav.Link
+                as={NavLink}
                 to="/"
                 end
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "nav-active" : ""}`
-                }
+                className={({ isActive }) => `nav-link ${isActive ? 'nav-active' : ''}`}
               >
                 Home
-              </NavLink>
+              </Nav.Link>
 
-              <NavLink
+              <Nav.Link
+                as={NavLink}
                 to="/about"
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "nav-active" : ""}`
-                }
+                className={({ isActive }) => `nav-link ${isActive ? 'nav-active' : ''}`}
               >
                 About
-              </NavLink>
+              </Nav.Link>
 
-              <NavLink
+              <Nav.Link
+                as={NavLink}
                 to="/contact"
-                className={({ isActive }) =>
-                  `nav-link ${isActive ? "nav-active" : ""}`
-                }
+                className={({ isActive }) => `nav-link ${isActive ? 'nav-active' : ''}`}
               >
                 Contact
-              </NavLink>
+              </Nav.Link>
 
               <NavDropdown title="Cakes" id="cakes-dropdown">
-                <LinkContainer to="/prebaked">
-                  <NavDropdown.Item>Prebaked Cakes</NavDropdown.Item>
-                </LinkContainer>
+                <NavDropdown.Item as={Link} to="/prebaked">
+                  Prebaked Cakes
+                </NavDropdown.Item>
 
-                <LinkContainer to="/ready-to-bake">
-                  <NavDropdown.Item>Personalised Cakes</NavDropdown.Item>
-                </LinkContainer>
+                <NavDropdown.Item as={Link} to="/ready-to-bake">
+                  Personalised Cakes
+                </NavDropdown.Item>
               </NavDropdown>
             </Nav>
 
-
-                {/* CENTER REVIEW MESSAGE */}
-<div className="nav-rating">
-  <span className="nav-rating__stars">
-    ★★★★★
-  </span>
-  <span className="nav-rating__text">
-    rated on Google & Facebook
-  </span>
-</div>
-
+            {/* CENTER REVIEW MESSAGE */}
+            <div className="nav-rating">
+              <span className="nav-rating__stars">★★★★★</span>
+              <span className="nav-rating__text">rated on Google & Facebook</span>
+            </div>
 
             {/* RIGHT NAV */}
             <Nav>
               {userInfo ? (
-                <NavDropdown title={userInfo.name || "User"} id="username">
-                  <LinkContainer to="/profile">
-                    <NavDropdown.Item>Profile</NavDropdown.Item>
-                  </LinkContainer>
+                <NavDropdown title={userInfo.name || 'User'} id="username" align="end">
+                  <NavDropdown.Item as={Link} to="/profile">
+                    Profile
+                  </NavDropdown.Item>
 
                   {userInfo.isAdmin && (
                     <>
                       <NavDropdown.Divider />
 
-                      <LinkContainer to="/admin/userlist">
-                        <NavDropdown.Item>Users</NavDropdown.Item>
-                      </LinkContainer>
+                      <NavDropdown.Item as={Link} to="/admin/userlist">
+                        Users
+                      </NavDropdown.Item>
 
-                      <LinkContainer to="/admin/productlist">
-                        <NavDropdown.Item>Products</NavDropdown.Item>
-                      </LinkContainer>
+                      <NavDropdown.Item as={Link} to="/admin/productlist">
+                        Products
+                      </NavDropdown.Item>
 
-                      <LinkContainer to="/admin/orderlist">
-                        <NavDropdown.Item>Orders</NavDropdown.Item>
-                      </LinkContainer>
+                      <NavDropdown.Item as={Link} to="/admin/orderlist">
+                        Orders
+                      </NavDropdown.Item>
                     </>
                   )}
 
                   <NavDropdown.Divider />
 
-                  <NavDropdown.Item onClick={logoutHandler}>
-                    Logout
-                  </NavDropdown.Item>
+                  <NavDropdown.Item onClick={logoutHandler}>Logout</NavDropdown.Item>
                 </NavDropdown>
               ) : (
-                <LinkContainer to="/login">
-                  <Nav.Link>
-                    <i className="fas fa-user"></i> Login
-                  </Nav.Link>
-                </LinkContainer>
+                <Nav.Link as={Link} to="/login">
+                  <i className="fas fa-user" /> Login
+                </Nav.Link>
               )}
 
-              <LinkContainer to="/cart">
-                <Nav.Link className="d-flex align-items-center">
-                  <i className="fas fa-shopping-cart"></i> Cart
-                  {cartItemCount > 0 && (
-                    <span className="cart-badge">{cartItemCount}</span>
-                  )}
-                </Nav.Link>
-              </LinkContainer>
+              <Nav.Link as={Link} to="/cart" className="d-flex align-items-center">
+                <i className="fas fa-shopping-cart" /> Cart
+                {cartItemCount > 0 && <span className="cart-badge">{cartItemCount}</span>}
+              </Nav.Link>
             </Nav>
           </Navbar.Collapse>
         </Container>
       </Navbar>
     </header>
-  );
+  )
 }
 
-export default Header;
+export default Header
