59d1584855d14030f12eecebca553ab1a8c2fa5c
tstyer <travisstyer.ts@gmail.com>
2026-03-08 16:59:42 +0000

    Add: delete review reducer

--- backend/frontend/src/reducers/productReducers.js
+++ backend/frontend/src/reducers/productReducers.js
@@ -115,3 +115,18 @@ export const productReviewCreateReducer = (state = {}, action) => {
       return state
   }
 }
+
+export const productReviewDeleteReducer = (state = {}, action) => {
+  switch (action.type) {
+    case PRODUCT_DELETE_REVIEW_REQUEST:
+      return { loading: true }
+    case PRODUCT_DELETE_REVIEW_SUCCESS:
+      return { loading: false, success: true }
+    case PRODUCT_DELETE_REVIEW_FAIL:
+      return { loading: false, error: action.payload }
+    case PRODUCT_DELETE_REVIEW_RESET:
+      return {}
+    default:
+      return state
+  }
+}
