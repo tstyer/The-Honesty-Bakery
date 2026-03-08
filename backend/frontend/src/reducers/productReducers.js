4f04f93e78f7d18aaebee69ebb22031ac68b3238
tstyer <travisstyer.ts@gmail.com>
2026-03-08 17:05:41 +0000

    Add imports to product reducers

--- backend/frontend/src/reducers/productReducers.js
+++ backend/frontend/src/reducers/productReducers.js
@@ -21,6 +21,10 @@ import {
     PRODUCT_CREATE_REVIEW_SUCCESS,
     PRODUCT_CREATE_REVIEW_FAIL,
     PRODUCT_CREATE_REVIEW_RESET,
+    PRODUCT_DELETE_REVIEW_REQUEST,
+    PRODUCT_DELETE_REVIEW_SUCCESS,
+    PRODUCT_DELETE_REVIEW_FAIL,
+    PRODUCT_DELETE_REVIEW_RESET,
  } from '../constants/productConstants'
 
 export const productListReducer = (state = { products: [] }, action) => {
