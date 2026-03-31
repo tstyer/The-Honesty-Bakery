e45e9673cd80637c1b25ddb76687f9e5b8617f21
tstyer <travisstyer.ts@gmail.com>
2026-03-31 14:45:31 +0100

    Add: error.css to app.js

--- backend/frontend/src/index.js
+++ backend/frontend/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
-import './product_card.css';
 
+// CSS
+import './product_card.css';
 import './index.css';
 import './about.css';
 import './products.css';
 import './cart.css';
 import './place_order.css';
 import './bootstrap.min.css';
+import './error.css';
+
+// APP
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
